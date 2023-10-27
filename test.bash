02fd04c5ea8dc79c24d4ea5e08c24e5328215056
Yamato Osada <yamatoosada86@gmail.com>
2023-10-28 01:34:06 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+ng () {
+      echo NG at Line $1
+      res=1
+}
+res=0　
+### I/O TEST ###
 out=$(seq 5 | ./plus)
-
-[ "${out}" = 14 ]
+[ "${out}" = 14 ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
