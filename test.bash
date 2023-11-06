2573cc17dd2daafc64427af0179cfceae84f3463
Yamato Osada <yamatoosada86@gmail.com>
2023-11-06 23:55:09 +0900

    fixed test.bash

--- test.bash
+++ test.bash
@@ -8,6 +8,6 @@ ng () {
 res=0　
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
