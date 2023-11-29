9dc5ca33952e29a8d324f49c61a81007fe339695
Yamato Osada <yamatoosada86@gmail.com>
2023-11-29 16:10:59 +0900

    fix test

--- README.md
+++ README.md
@@ -3,14 +3,36 @@
 
 ## インストール方法 
 このリポジトリを任意の場所にクローンして使用。
+```
+git clone https://github.com/YamatoOsada/robosys2023.git
+```
+
 ## plus
 標準入力から読み込んだ数字を足す。
+### 実行例
+```
+$ seq 10 | ./plus
+55
+```
+seq 10で出力した数字を1から10まで順に足している。
 
 ## minus
 標準入力から読み込んだ数字を引く。
+### 実行例
+```
+$ seq 10 | ./minus
+-55
+```
+seq 10で出力した数字を1から10まで順に引いている。
 
 ## mul
 標準入力から読み込んだ数字を掛ける。
+### 実行例
+```
+$ seq 10 | ./mul
+3628800
+```
+seq 10で出力した数字を1から10まで順に掛けている。
 
 ## 必要なソフトウェア
 * Python
@@ -19,9 +41,9 @@
 ## テスト環境
 * Ubuntu 22.04.3 LTS
 
-## 著作権ライセンス 
-© 2022 Ryuichi Ueda* このソフトウェアパッケージは，3条項BSDライセンスの下，再頒布および使用が許可されます．
-* © 2023 Yamato Osada
+## 著作権, ライセンス
 * このソフトウェアパッケージは，3条項BSDライセンスの下，再頒布および使用が許可されます．
 * このパッケージのコードは，下記のスライド（CC-BY-SA 4.0 by Ryuichi Ueda）のものを，本人の許可を得て自身の著作としたものです．
      * [ryuichiueda/my_slides robosys_2022 lesson4](https://github.com/ryuichiueda/my_slides/blob/master/robosys_2022/lesson4.md)
+     * © 2022 Ryuichi Ueda
+* © 2023 Yamato Osada
--- test.bash
+++ test.bash
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2023 Yamato Osada
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
+ng(){
 	echo NG at Line $1
 	res=1
 }
@@ -11,56 +11,56 @@ res=0
 ### I/O ###
 #plus
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng${LINENO}
 
 #minus
 out=$(seq -5 | ./minus)
-[ "${out}" = -15 ] || ng ${LINENO}
+[ "${out}" = -15 ] || ng${LINENO}
 
 #mul
 out=$(seq 5 | ./mul)
-[ "${out}" = 120 ] || ng ${LINENO}
+[ "${out}" = 120 ] || ng${LINENO}
 
 
 ### STRANGE INPUT ###
 #plus
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
-out=$(echo & | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo ? | ./plus)
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
 out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
 #minus
 out=$(echo あ | ./minus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
-out=$(echo & | ./minus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo ? | ./minus)
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
 out=$(echo | ./minus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
 #mul
 out=$(echo あ | ./mul)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
-out=$(echo & | ./mul)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo ? | ./mul)
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
 out=$(echo | ./mul) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng${LINENO}
+[ "${out}" = "" ] || ng${LINENO}
 
 
 [ "$res" = 0 ] && echo OK
